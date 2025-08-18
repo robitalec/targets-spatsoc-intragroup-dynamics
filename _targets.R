8f205bc0c7a36d7ae357d63cfb6b0bc7b2f6ddae
Alec L. Robitaille <robit.alec@gmail.com>
2025-08-18 15:57:39 -0300

    variables

--- _targets.R
+++ _targets.R
@@ -20,3 +20,19 @@ tar_source('R')
 # Targets options ---------------------------------------------------------
 tar_option_set(format = 'qs')
 
+
+# Variables --------------------------------------------------------------
+filepath <- file.path('input', 'DT.csv')
+temporal_threshold <- '10 minutes'
+spatial_threshold <- 50
+
+datetime <- 'datetime'
+id <- 'ID'
+coords <- c('X', 'Y')
+projection <- 32736
+
+timegroup <- 'timegroup'
+group <- 'group'
+
+
+
