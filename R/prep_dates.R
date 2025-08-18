5b470d3dbec7b81050a3adc5ff59347111a858e6
Alec L. Robitaille <robit.alec@gmail.com>
2025-08-18 15:58:41 -0300

    prep_dates function

---
+++ R/prep_dates.R
@@ -0,0 +1,26 @@
+#' Prepare datetime columns
+#'
+#' @param DT data.table
+#' @param datetime character; datetime column name
+#'
+#' @return
+#' data.table with datetime columns added
+#' @export
+#'
+#' @examples
+#' path <- system.file('extdata', 'DT.csv', package = 'spatsoc')
+#' DT <- read_data(path = path)
+#' prep_dates(DT, 'datetime')
+prep_dates <- function(DT, datetime) {
+  if (!datetime %in% colnames(DT)) {
+    stop('datetime not found in DT')
+  }
+
+  DT[, datetime := as.POSIXct(datetime), env = list(datetime = datetime)]
+
+  DT[, doy := data.table::yday(datetime)]
+  DT[, yr := data.table::year(datetime)]
+  DT[, mnth := data.table::month(datetime)]
+
+  return(DT)
+}
